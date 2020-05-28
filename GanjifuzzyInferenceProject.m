693ef89f60815d2eea5e9e6129a3ec181e42868e
ganjalipour <ibrahim.ganjali@gmail.com>
2020-05-28 17:45:28 +0430

    remove not related images

--- GanjifuzzyInferenceProject.m
+++ GanjifuzzyInferenceProject.m
@@ -14,33 +14,28 @@ inf=99999;
 
 %% adding variables
 fis = addvar(fis,'input','Velocity_Soraat',[0 180]);
-fis = addmf(fis,'input',1,'LVelocity','trapmf', [-inf -inf 30 50]);
-fis = addmf(fis,'input',1,'MVelocity','trapmf', [30 50 70 90]);
-fis = addmf(fis,'input',1,'HVelocity','trapmf', [70 90 inf inf]);
+fis = addmf(fis,'input',1,'LVelocity','trimf', [-inf 30 70]);
+fis = addmf(fis,'input',1,'HVelocity','trimf', [30 70 inf]);
+
 
 fis = addvar(fis,'input','Distance_fasele', [0 50]);
-fis = addmf(fis,'input',2,'LDistance','trapmf', [-inf -inf 10 20]);
-fis = addmf(fis,'input',2,'MDistance','trapmf', [10 20 30 40]);
-fis = addmf(fis,'input',2,'HDistance','trapmf', [30 40 inf inf]);
+fis = addmf(fis,'input',2,'LDistance','trimf', [-inf 10 30]);
+fis = addmf(fis,'input',2,'HDistance','trimf', [10 30 inf]);
 
 
-fis = addvar(fis,'output','Acceleration', [-1 1]);
-fis = addmf(fis,'output',1,'NL','trapmf', [-inf -inf -0.6 -0.2]);
-fis = addmf(fis,'output',1,'NS','trimf', [-0.6 -0.2 0]);
-fis = addmf(fis,'output',1,'Zero','trimf', [-0.2 0 0.2]);
-fis = addmf(fis,'output',1,'PS','trimf', [0 0.2 0.6]);
-fis = addmf(fis,'output',1,'PL','trapmf', [0.2 0.6 inf inf]);
+
+fis = addvar(fis,'output','Acceleration', [-30 30]);
+
+fis = addmf(fis,'output',1,'NS','trimf', [-inf -10 0]);
+fis = addmf(fis,'output',1,'Zero','trimf', [-10 0 10]);
+fis = addmf(fis,'output',1,'PS','trimf', [0 10 inf]);
+
 
 
 %% add rules
-Rules = [1 1 3 1 1
-    1 2 4 1 1
-    1 3 5 1 1
-    2 1 2 1 1
-    2 2 3 1 1
-    2 3 4 1 1
-    3 1 1 1 1
-    3 2 2 1 1
-    3 3 3 1 1];
+Rules = [1 1 2 1 1
+    1 2 1 1 1
+    2 1 3 1 1
+    2 2 2 1 1];
 
 fis = addrule(fis,Rules);
